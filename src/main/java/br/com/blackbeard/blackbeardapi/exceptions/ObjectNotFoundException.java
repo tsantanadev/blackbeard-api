f96e2f153eaceb8ca0baf12fa98e1db488eac5a5
Thiago Santana <thiago91098@gmail.com>
2021-07-31 19:31:03 -0300

    Improving Exception handler

--- src/main/java/br/com/blackbeard/blackbeardapi/exceptions/ObjectNotFoundException.java
+++ src/main/java/br/com/blackbeard/blackbeardapi/exceptions/ObjectNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.blackbeard.blackbeardapi.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ObjectNotFoundException extends RuntimeException {
 
     private static final String OBJECT_NOT_FOUND = "Object not found";
--- src/main/java/br/com/blackbeard/blackbeardapi/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/blackbeard/blackbeardapi/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,5 @@
 package br.com.blackbeard.blackbeardapi.exceptions;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,26 +8,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Calendar;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardError<List<String>>> validation(
+    public ResponseEntity<StandardError<Map<String, String>>> validation(
             MethodArgumentNotValidException e, HttpServletRequest request) {
-        StandardError<List<String>> error =
-                new StandardError<>(
-                        HttpStatus.BAD_REQUEST.value(),
-                        e.getBindingResult().getAllErrors().stream()
-                                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                                .collect(Collectors.toList()),
-                        "Validation error",
-                        Calendar.getInstance().getTimeInMillis(),
-                        request.getRequestURI());
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        var errors = new HashMap<String, String>();
+
+        e.getFieldErrors().forEach(fieldError -> errors.put(fieldError.getField(),
+                fieldError.getDefaultMessage() != null ? fieldError.getDefaultMessage() : ""));
+
+        return ResponseEntity.badRequest().body(new StandardError<>(
+                HttpStatus.BAD_REQUEST.value(),
+                errors,
+                "Validation error",
+                Calendar.getInstance().getTimeInMillis(),
+                request.getRequestURI()));
     }
 
 }
