bb6ab3635aa63d43dd1125981fc65a740dc7da36
Thiago Santana <thiago91098@gmail.com>
2021-08-02 09:21:58 -0300

    Add integration tests

--- src/test/java/br/com/blackbeard/blackbeardapi/controllers/BarberShopControllerTest.java
+++ src/test/java/br/com/blackbeard/blackbeardapi/controllers/BarberShopControllerTest.java
@@ -12,18 +12,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Map;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BarberShopController.class)
 class BarberShopControllerTest {
@@ -86,6 +90,90 @@ class BarberShopControllerTest {
                 .content(json))
                 .andExpect(status().isCreated())
                 .andExpect(content().string(this.mapper.writeValueAsString(barberShopResponseExpected)));
+    }
+
+    @Test
+    void shouldReturnBadRequestAndErrorListWhenPostAnInvalidRequest() throws Exception {
+        var barberShopRequest = BarberShopRequest.builder().build();
+
+        var errors = Map.of(
+                "address", "must not be null",
+                "imageUrl", "must not be blank",
+                "name", "must not be blank"
+        );
+
+        var requestJson = mapper.writeValueAsString(barberShopRequest);
+
+        mockMvc.perform(post("/barberShop")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", is(errors)))
+                .andExpect(jsonPath("$.status", is(HttpStatus.BAD_REQUEST.value())));
+    }
+
+    @Test
+    void shouldReturnAcceptWhenPutAValidRequest() throws Exception {
+        var addressRequest = AddressRequest.builder()
+                .city("Test")
+                .district("Test")
+                .street("Test")
+                .number("42")
+                .build();
+
+
+        var barberShopRequest = BarberShopRequest.builder()
+                .name("Test")
+                .imageUrl("https://www.google.com")
+                .address(addressRequest)
+                .build();
+
+        var address = Address.builder()
+                .city(addressRequest.getCity())
+                .district(addressRequest.getDistrict())
+                .street(addressRequest.getStreet())
+                .number(addressRequest.getNumber())
+                .build();
+
+        var barberShop = BarberShop.builder()
+                .name(barberShopRequest.getName())
+                .imageUrl(barberShopRequest.getImageUrl())
+                .address(address)
+                .build();
+
+        var barberShopId = UUID.randomUUID();
+
+        var json = mapper.writeValueAsString(barberShopRequest);
+
+        mockMvc.perform(put("/barberShop")
+                        .param("barberShopId", barberShopId.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isAccepted());
+
+        verify(service).update(barberShop, barberShopId);
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenPutAnInvalidRequest() throws Exception {
+        var barberShopRequest = BarberShopRequest.builder().build();
+        var barberShopId = UUID.randomUUID();
+
+        var errors = Map.of(
+                "address", "must not be null",
+                "imageUrl", "must not be blank",
+                "name", "must not be blank"
+        );
+
+        var requestJson = mapper.writeValueAsString(barberShopRequest);
+
+        mockMvc.perform(put("/barberShop")
+                        .param("barberShopId", barberShopId.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", is(errors)))
+                .andExpect(jsonPath("$.status", is(HttpStatus.BAD_REQUEST.value())));
     }
 
     @Test
