f008aecae0b607beb598296c65a52ddebc8ee225
Welbert Fernandes <welbert.jsj@gmail.com>
2021-10-08 13:02:12 -0600

    alter version

--- src/main/java/br/com/blackbeard/blackbeardapi/BlackbeardApiApplication.java
+++ src/main/java/br/com/blackbeard/blackbeardapi/BlackbeardApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlackbeardApiApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("user.timezone", "America/Sao_Paulo");
 		SpringApplication.run(BlackbeardApiApplication.class, args);
 	}
 
